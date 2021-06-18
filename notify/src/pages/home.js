c5354e1e2147a651ba755ba14387ce37675487d8
isheekamitra <2019ugee003@nitjsr.ac.in>
2021-06-18 14:42:57 +0530

    final hosting

--- notify/src/pages/home.js
+++ notify/src/pages/home.js
@@ -1,10 +1,25 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Posts from "../components/home/Posts"
 import Rightsidebar from "../components/home/Rightsidebar";
 import Status from "../components/home/Status"
+let scroll=0;
 
 const Home = () => {
   const{homepost}=useSelector(state=>state)
+  window.addEventListener('scroll',()=>{
+    if(window.location.pathname==='/')
+    {
+      scroll=window.pageYOffset
+      return scroll;
+    }
+  })
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      window.scrollTo({top:scroll,behavior:'smooth'})
+    },100)
+  },[])
     return (
         <div className="home row mx-0">
         <div className="col-md-8">
