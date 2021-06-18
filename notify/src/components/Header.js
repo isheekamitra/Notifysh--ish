4175d1a0f32f55b47d4248a3c609087c41bd4d29
isheekamitra <2019ugee003@nitjsr.ac.in>
2021-06-18 13:31:30 +0530

    changes

--- notify/src/components/Header.js
+++ notify/src/components/Header.js
@@ -22,7 +22,8 @@ const Header = () => {
     }
     return (
         <div className="header bg-light">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between align-middle">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light 
+        justify-content-between align-middle">
   <Link className="logo" to="/">
   <h1 className="navbar-brand text-uppercase p-0 m-0"
    onClick={()=>window.scrollTo({top:0})}
